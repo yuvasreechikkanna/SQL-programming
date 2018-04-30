4b6f083294a19e29464aad09036b8045163efb94
yuvasreechikkanna <37336105+yuvasreechikkanna@users.noreply.github.com>
2018-04-30 09:18:22 +0530

    Create create database and table.sql

---
+++ beginner/create database and table.sql
@@ -0,0 +1,4 @@
+create database students;
+show databases;
+use students;
+create table details(rollno varchar(8),name char(50),id int(8),email varchar(16),rank int,mark1 int,mark2 int,mark3 int,mark4 int,total int);
