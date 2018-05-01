9673de6ba1ef951ec3d7d14c3ec43d278d1ebd68
yuvasreechikkanna <37336105+yuvasreechikkanna@users.noreply.github.com>
2018-05-01 11:18:25 +0530

    Create index.sql

---
+++ beginner/index.sql
@@ -0,0 +1,12 @@
+create database students;
+show databases;
+use students;
+create table guvi(userid varchar(8),name char(50),toprank int(3));
+describe guvi;
+insert into guvi (userid,name,toprank) values("15bee043","yuvasree",4);
+insert into guvi (userid,name,toprank) values("15bee037","muunthan",3);
+insert into guvi (userid,name,toprank) values("15bee084","rahasudha",2);
+select * from guvi;
+CREATE INDEX idx_toprank ON guvi (toprank);
+select*from guvi;
+DROP INDEX guvi.idx_toprank;
